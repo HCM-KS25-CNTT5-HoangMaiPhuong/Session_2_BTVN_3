b1e7279f341ea1d4255758f8f382101498d85e7d
Hoàng Mai Phương <hoangmaiphuongtin@gmail.com>
2026-04-23 09:22:11 +0700

    Update baitap3.sql

--- baitap3.sql
+++ baitap3.sql
@@ -1,1 +1,14 @@
-SELECT * FROM students.orders;
+CREATE TABLE CUSTOMERS (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+CREATE TABLE ORDERS (
+    order_id INT PRIMARY KEY,
+    order_date DATE DEFAULT CURRENT_DATE,
+    total_amount DECIMAL(10,2) NOT NULL,
+    customer_id INT,
+    
+    CONSTRAINT fk_customer
+    FOREIGN KEY (customer_id)
+    REFERENCES CUSTOMERS(customer_id)
+);
